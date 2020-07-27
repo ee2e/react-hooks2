c406d9256196764fe016afaa25e78b48b12ff683
이지은 <ee2e@naver.com>
2020-07-27 09:48:29 +0900

    useInput

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { useState } from "react";
 import "./App.css";
 
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    console.log(event.target);
+  };
+  return { value, onChange };
+};
+
 const App = () => {
   const [item, setItem] = useState(1);
   const incrementItem = () => setItem(item + 1);
   const decrementItem = () => setItem(item - 1);
+
+  const name = useInput("Mr.");
   return (
     <div className="App">
       <h1>Hello {item}</h1>
       <h2>Start editing to see some magic happen!</h2>
       <button onClick={incrementItem}>Increment</button>
       <button onClick={decrementItem}>Decrement</button>
+      <hr />
+      {/* ...name : name 안에 있는 모든 것들을 보여줌 (여기서는 value={name}.value onChange={name.onChange}와 같다) */}
+      <input placeholder="Name" {...name} />
     </div>
   );
 };
