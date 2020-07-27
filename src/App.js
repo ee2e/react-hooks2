5fd0cb05b070e6ff860f59a0532fbde7d2a5c0a2
이지은 <ee2e@naver.com>
2020-07-27 11:38:58 +0900

    useTabs

--- src/App.js
+++ src/App.js
@@ -1,74 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    let willUpdate = true;
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
-    if (willUpdate) {
-      setValue(value);
-    }
-  };
-  return { value, onChange };
-};
+import UseState from "./Components/UseState";
+import UseInput from "./Components/UseInput";
+import UseTabs from "./Components/UseTabs";
 
 const App = () => {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
-
-  const maxLen = (value) => value.length <= 10;
-  // const maxLen = (value) => !value.includes("@");
-  const name = useInput("Mr.", maxLen);
-
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>Increment</button>
-      <button onClick={decrementItem}>Decrement</button>
+      <UseState />
       <hr />
-      {/* ...name : name 안에 있는 모든 것들을 보여줌 (여기서는 value={name}.value onChange={name.onChange}와 같다) */}
-      <input placeholder="Name" {...name} />
+      <UseInput />
+      <hr />
+      <UseTabs />
     </div>
   );
 };
-
-// class AppUgly extends React.Component {
-//   state = {
-//     item: 1,
-//   };
-//   render() {
-//     const { item } = this.state;
-//     return (
-//       <div className="App">
-//         <h1>Hello {item}</h1>
-//         <h2>Start editing to see some magic happen!</h2>
-//         <button onClick={this.incrementItem}>Increment</button>
-//         <button onClick={this.decrementItem}>Decrement</button>
-//       </div>
-//     );
-//   }
-//   incrementItem = () => {
-//     this.setState((state) => {
-//       return {
-//         item: state.item + 1,
-//       };
-//     });
-//   };
-//   decrementItem = () => {
-//     this.setState((state) => {
-//       return {
-//         item: state.item - 1,
-//       };
-//     });
-//   };
-// }
 
 export default App;
